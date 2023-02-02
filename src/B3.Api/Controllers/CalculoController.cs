f3d9e0173de2cf9d838b0712200e1ea9afc06238
Helio Gatts Jr <heliogatts@hotmail.com>
2023-02-02 15:43:05 -0300

    commit inicial projeto backend solucao

---
+++ src/B3.Api/Controllers/CalculoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B3.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculoController : ControllerBase
+    {
+    }
+}
